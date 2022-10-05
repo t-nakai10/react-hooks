94c1e38c426708c003370752b83f0fc9f80b6aa1
t-nakai10 <bad.sideline.999@gmail.com>
2022-10-05 21:31:52 +0900

    useState の基本

--- src/App.js
+++ src/App.js
@@ -1,23 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import "./App.css";
 
 function App() {
+  const [count, setCount] = useState(0);
+  const handleCount = () => {
+    setCount(count + 1);
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h2>UseState</h2>
+      <p>{count}</p>
+      <button onClick={handleCount}>＋</button>
+      <hr />
+      <h2>useContext</h2>
     </div>
   );
 }
