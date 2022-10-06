0738b227183c3ed875caa6b57ec381ef0e17c956
t-nakai10 <bad.sideline.999@gmail.com>
2022-10-06 23:14:46 +0900

    useReducer を使って値を更新する

--- src/App.js
+++ src/App.js
@@ -1,6 +1,38 @@
-import { useContext, useRef, useState } from "react";
+import { useContext, useReducer, useRef, useState } from "react";
 import TestInfoContext from ".";
 import "./App.css";
+
+// state が持つオブジェクトを定義しておく.
+const initialState = {
+  savings: 10000,
+  histories: [{ operation: "", amount: 0 }],
+};
+
+const reducer = (state, action) => {
+  // 基本的に action.type
+  switch (action.type) {
+    case "DEPOSIT":
+      return {
+        ...state,
+        savings: state.savings + action.payload,
+        histories: [
+          ...state.histories,
+          { operation: "預け入れ", amount: action.payload },
+        ],
+      };
+    case "WITHDRAW":
+      return {
+        ...state,
+        savings: state.savings - action.payload,
+        histories: [
+          ...state.histories,
+          { operation: "引き出し", amount: action.payload },
+        ],
+      };
+    default:
+      return state;
+  }
+};
 
 function App() {
   const [count, setCount] = useState(0);
@@ -14,6 +46,21 @@ function App() {
   const ref = useRef();
   const handleRef = () => {
     console.log(ref.current.value);
+  };
+
+  // useReducer(state を更新するための関数, 初期値).
+  // 前の state に基づいて次の state を決めるときに必要.
+  const [state, dispatch] = useReducer(reducer, initialState);
+  const [amount, setAmount] = useState(0);
+
+  // それぞれ預け入れ引き出し時に発火, 引数はinputに入力された金額.
+  const onDeposit = (amount) => {
+    dispatch({ type: "DEPOSIT", payload: amount });
+    setAmount(0);
+  };
+  const onWithdraw = (amount) => {
+    dispatch({ type: "WITHDRAW", payload: amount });
+    setAmount(0);
   };
 
   return (
@@ -35,6 +82,30 @@ function App() {
       <hr />
 
       <h2>useReducer</h2>
+      <h3>残高 : {state.savings.toLocaleString()}</h3>
+      <div>
+        <input
+          value={amount}
+          onChange={(e) => {
+            setAmount(Number(e.target.value));
+          }}
+          type="number"
+        />
+      </div>
+      <div>
+        <button onClick={() => onDeposit(amount)}>預け入れ</button>
+        <button onClick={() => onWithdraw(amount)}>引き出し</button>
+      </div>
+      <div>
+        <p>履歴</p>
+        {state.histories.map((history, index) => {
+          return (
+            <p key={index}>
+              {history.operation}: {history.amount}
+            </p>
+          );
+        })}
+      </div>
     </div>
   );
 }
