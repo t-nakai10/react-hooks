6fca28145f2e733faa6160ecdef730cd475a86c0
t-nakai10 <bad.sideline.999@gmail.com>
2022-10-07 20:05:33 +0900

    コンポーネント化によるApp.jsの更新

--- src/App.js
+++ src/App.js
@@ -1,42 +1,31 @@
-import { useContext, useReducer, useRef, useState } from "react";
-import TestInfoContext from ".";
+import React from "react";
 import "./App.css";
+import Context from "./hooks/Context";
 import Reducer from "./hooks/Reducer";
+import Ref from "./hooks/Ref";
+import State from "./hooks/State";
+import Memo from "./hooks/Memo";
 
 function App() {
-  const [count, setCount] = useState(0);
-  const handleCount = () => {
-    setCount(count + 1);
-  };
-
-  const testInfo = useContext(TestInfoContext);
-
-  // useRef() 対象の要素を参照できる.
-  const ref = useRef();
-  const handleRef = () => {
-    console.log(ref.current.value);
-  };
-
   return (
     <div className="App">
       <h2>UseState</h2>
-      <p>{count}</p>
-      <button onClick={handleCount}>＋</button>
-      <hr />
+      <State />
 
       <h2>useContext</h2>
-      <p>{testInfo.name}</p>
-      <p>{testInfo.age}</p>
+      <Context />
+
       <hr />
 
       <h2>useRef</h2>
-      <input ref={ref} onChange={handleRef} />
-      {/* ボタンを押したときも. */}
-      <button onClick={handleRef}>Ref</button>
+      <Ref />
       <hr />
 
       <h2>useReducer</h2>
       <Reducer />
+
+      <h2>useMemo</h2>
+      <Memo />
     </div>
   );
 }
